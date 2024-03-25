266c56d4c3cfba839fa7ae71bfba9eafddd59820
Aditya Prayoga <aditya@brainmatics.com>
2024-03-25 07:31:04 +0700

    initial project

--- src/main/java/id/go/kemenkeu/anggaran/todoapi/security/BCrypt.java
+++ src/main/java/id/go/kemenkeu/anggaran/todoapi/security/BCrypt.java
@@ -12,7 +12,7 @@
 // ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 // OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-package anggaran.todoapi.security;
+package id.go.kemenkeu.anggaran.todoapi;
 
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
